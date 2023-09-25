745d7d33025c5257ac62058f45ff405078f2b305
Alex Forsythe <awforsythe@gmail.com>
2023-09-25 11:48:22 -0400

    Simplify postgres init

--- postgres.tf
+++ postgres.tf
@@ -1,48 +1,24 @@
-variable postgres_database_name {
-  description = "Name of the database created in PostgreSQL to contain Golden VCR's persistent data"
-  default     = "gvcr"
-}
-
-variable postgres_showtime_schema_name {
-  description = "Name of the schema that contains the data owned by the showtime API"
-  default     = "showtime"
-}
-
-variable postgres_showtime_user_name {
-  description = "Name of the PostgreSQL user/role that can access the showtime schema"
-  default     = "showtime"
-}
-
-resource "random_password" "postgres_showtime_password" {
+resource "random_password" "postgres_tapes_password" {
   keepers = {
-    schema   = var.postgres_showtime_schema_name
-    username = var.postgres_showtime_user_name
     version  = 1
   }
   length = 32
 }
 
-output "postgres_init_script" {
+resource "random_password" "postgres_showtime_password" {
+  keepers = {
+    version  = 1
+  }
+  length = 32
+}
+
+output "tapes_db_env" {
   value     = <<EOT
-#!/usr/bin/env bash
-set -e
-
-echo "Checking for existing database named '${var.postgres_database_name}'..."
-HAS_DATABASE=$(sudo -u postgres psql -qtAX -c "SELECT COUNT(*) FROM pg_database WHERE datname = '${var.postgres_database_name}'")
-if [ $HAS_DATABASE -eq 0 ]; then
-  echo "Creating new database..."
-  sudo -u postgres psql -qtAX -c "CREATE DATABASE ${var.postgres_database_name}"
-fi
-
-echo "Initializing database user '${var.postgres_showtime_user_name}' with access to schema '${var.postgres_showtime_schema_name}'..."
-HAS_SHOWTIME_USER=$(sudo -u postgres psql -qtAX -c "SELECT COUNT(*) FROM pg_catalog.pg_roles WHERE rolname = '${var.postgres_showtime_user_name}'")
-if [ $HAS_SHOWTIME_USER -eq 0 ]; then
-  echo "Creating new database user..."
-  sudo -u postgres psql -qtAX -c "CREATE ROLE ${var.postgres_showtime_user_name} WITH LOGIN PASSWORD '${random_password.postgres_showtime_password.result}'"
-fi
-sudo -u postgres psql -d ${var.postgres_database_name} -qtAX -c "CREATE SCHEMA IF NOT EXISTS ${var.postgres_showtime_schema_name} AUTHORIZATION ${var.postgres_showtime_user_name}"
-
-echo "Database initialized."
+PGHOST=127.0.0.1
+PGPORT=5432
+PGDATABASE=tapes
+PGUSER=tapes
+PGPASSWORD=${random_password.postgres_tapes_password.result}
 EOT
   sensitive = true
 }
@@ -50,9 +26,44 @@ EOT
 output "showtime_db_env" {
   value     = <<EOT
 PGHOST=127.0.0.1
-PGDATABASE=${var.postgres_database_name}
-PGUSER=${var.postgres_showtime_user_name}
+PGPORT=5432
+PGDATABASE=showtime
+PGUSER=showtime
 PGPASSWORD=${random_password.postgres_showtime_password.result}
+EOT
+  sensitive = true
+}
+
+output "postgres_init_script" {
+  value     = <<EOT
+#!/usr/bin/env bash
+set -e
+
+init_db() {
+  DATABASE_NAME="$1"
+  SCHEMA_NAME="$1"
+  USER_NAME="$1"
+  PASSWORD="$2"
+
+  echo "Checking for existing database named $DATABASE_NAME..."
+  HAS_DATABASE=$(sudo -u postgres psql -qtAX -c "SELECT COUNT(*) FROM pg_database WHERE datname = '$DATABASE_NAME'")
+  if [ $HAS_DATABASE -eq 0 ]; then
+    echo "Creating new database..."
+    sudo -u postgres psql -qtAX -c "CREATE DATABASE $DATABASE_NAME"
+  fi
+
+  echo "Initializing database user '$USER_NAME' with access to schema '$SCHEMA_NAME'..."
+  HAS_USER=$(sudo -u postgres psql -qtAX -c "SELECT COUNT(*) FROM pg_catalog.pg_roles WHERE rolname = '$USER_NAME'")
+  if [ $HAS_USER -eq 0 ]; then
+    echo "Creating new database user..."
+    sudo -u postgres psql -qtAX -c "CREATE ROLE $USER_NAME WITH LOGIN PASSWORD '$PASSWORD'"
+  fi
+  sudo -u postgres psql -d $DATABASE_NAME -qtAX -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA_NAME AUTHORIZATION $USER_NAME"
+}
+
+init_db 'tapes' '${random_password.postgres_tapes_password.result}'
+init_db 'showtime' '${random_password.postgres_showtime_password.result}'
+echo "Database initialized."
 EOT
   sensitive = true
 }
