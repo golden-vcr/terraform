7727cdf148acc8a8f19825b2f0069497d0e07d04
Alex Forsythe <awforsythe@gmail.com>
2023-09-27 15:18:07 -0400

    Fix mount-volume.sh to actually detect mount, not just existence of dir

--- server-init/mount-volume.sh
+++ server-init/mount-volume.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-set -e
 
 GVCR_DATA_VOLUME=/dev/disk/by-id/scsi-0DO_Volume_gvcr-data
 GVCR_DATA_MOUNT=/mnt/gvcr_data
 
-if [ -d $GVCR_DATA_MOUNT ]; then
+findmnt "$GVCR_DATA_MOUNT"
+if [ "$?" == "0" ]; then
     echo "$GVCR_DATA_VOLUME is mounted at $GVCR_DATA_MOUNT."
     exit
 fi
 
+set -e
 echo "Creating $GVCR_DATA_MOUNT and mounting $GVCR_DATA_VOLUME..."
-mkdir -p /mnt/gvcr_data
-mount -o discard,defaults,noatime $GVCR_DATA_VOLUME $GVCR_DATA_MOUNT
-echo "$GVCR_DATA_VOLUME $GVCR_DATA_MOUNT ext4 defaults,nofail,discard 0 0" | tee /etc/fstab
+rm -rf "$GVCR_DATA_MOUNT"
+mkdir -p "$GVCR_DATA_MOUNT"
+mount -o defaults,nofail,discard,noatime $GVCR_DATA_VOLUME $GVCR_DATA_MOUNT
+echo "$GVCR_DATA_VOLUME $GVCR_DATA_MOUNT ext4 defaults,nofail,discard,noatime 0 0" | tee /etc/fstab
+findmnt "$GVCR_DATA_MOUNT"
 echo "Mounted volume."
