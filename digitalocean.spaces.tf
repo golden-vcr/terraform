386e5f911c8f1a8be49fd45378bf3fba83246d2c
Alex Forsythe <awforsythe@gmail.com>
2023-10-19 19:16:57 -0400

    Add spaces bucket for storing images generated from user prompts

--- digitalocean.spaces.tf
+++ digitalocean.spaces.tf
@@ -1,17 +1,27 @@
+// Bucket where we deploy static builds of the frontend app
 resource "digitalocean_spaces_bucket" "frontend" {
   name   = "golden-vcr-frontend"
   region = "nyc3"
   acl    = "public-read"
 }
 
+// Bucket where we deploy static builds of the graphics app (i.e. OBS overlay)
 resource "digitalocean_spaces_bucket" "graphics" {
   name   = "golden-vcr-graphics"
   region = "nyc3"
   acl    = "public-read"
 }
 
+// Bucket where we upload scanned images for each tape
 resource "digitalocean_spaces_bucket" "images" {
   name   = "golden-vcr-images"
+  region = "nyc3"
+  acl    = "public-read"
+}
+
+// Bucket where we upload images generated from user submissions during streams
+resource "digitalocean_spaces_bucket" "user_images" {
+  name   = "golden-vcr-user-images"
   region = "nyc3"
   acl    = "public-read"
 }
@@ -43,6 +53,17 @@ output "images_s3_env" {
 SPACES_BUCKET_NAME=${digitalocean_spaces_bucket.images.name}
 SPACES_REGION_NAME=${digitalocean_spaces_bucket.images.region}
 SPACES_ENDPOINT_URL=${digitalocean_spaces_bucket.images.endpoint}
+SPACES_ACCESS_KEY_ID=${var.digitalocean_spaces_key_id}
+SPACES_SECRET_KEY=${var.digitalocean_spaces_secret}
+EOT
+  sensitive = true
+}
+
+output "user_images_s3_env" {
+  value     = <<EOT
+SPACES_BUCKET_NAME=${digitalocean_spaces_bucket.user_images.name}
+SPACES_REGION_NAME=${digitalocean_spaces_bucket.user_images.region}
+SPACES_ENDPOINT_URL=${digitalocean_spaces_bucket.user_images.endpoint}
 SPACES_ACCESS_KEY_ID=${var.digitalocean_spaces_key_id}
 SPACES_SECRET_KEY=${var.digitalocean_spaces_secret}
 EOT
